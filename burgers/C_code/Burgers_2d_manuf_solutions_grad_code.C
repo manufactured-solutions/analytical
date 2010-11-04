7ad6f3407c29ae72984f96c1b5570bd3f9f1c7b4
Kemelli C. Estacio-Hiroms <kemelli@ices.utexas.edu>
2010-11-04 21:35:47 +0000

    MMS: Removing former folder burgers. It used Salari&Knupp's manufactured solutions and we are changing to Roy et al(2002)'s solutions. New folder for Burgers will be added in sequence.

--- burgers/C_code/Burgers_2d_manuf_solutions_grad_code.C
+++
@@ -1,6 +0,0 @@
-u_an = u_0 * (sin(x * x + y * y + omega * t) + epsilon);
-v_an = v_0 * (cos(x * x + y * y + omega * t) + epsilon);
-grad_u_an[0] = 0.2e1 * u_0 * cos(x * x + y * y + omega * t) * x;
-grad_u_an[1] = 0.2e1 * u_0 * cos(x * x + y * y + omega * t) * y;
-grad_v_an[0] = -0.2e1 * v_0 * sin(x * x + y * y + omega * t) * x;
-grad_v_an[1] = -0.2e1 * v_0 * sin(x * x + y * y + omega * t) * y;
