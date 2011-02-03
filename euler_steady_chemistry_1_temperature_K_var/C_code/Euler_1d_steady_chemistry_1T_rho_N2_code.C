dc5efeea3215c2bf2e1c3011bb5bf810e62c13c6
Kemelli C. Estacio-Hiroms <kemelli@ices.utexas.edu>
2011-02-03 22:42:34 +0000

    MMS: Adding folder for 1D steady Euler + chemistry in thermal equilibrium (only one temperature). Equibibrium constant K is a to-be-defined function of T: K=K(T).

---
+++ euler_steady_chemistry_1_temperature_K_var/C_code/Euler_1d_steady_chemistry_1T_rho_N2_code.C
@@ -0,0 +1,34 @@
+#include <math.h>
+
+double SourceQ_rho_N2 (
+  double x,
+  double M_N,
+  double h0_N,
+  double h0_N2,
+  double K,
+  double Cf1_N,
+  double Cf1_N2,
+  double etaf1_N,
+  double etaf1_N2,
+  double Ea_N,
+  double Ea_N2,
+  double Function_to_Calculate_K)
+{
+  double Q_rho_N2;
+  double RHO_N;
+  double RHO_N2;
+  double U;
+  double T;
+  double kf1_N;
+  double kf1_N2;
+  double K_eq;
+  K_eq = Function_to_Calculate_K;
+  RHO_N = rho_N_0 + rho_N_x * sin(a_rho_N_x * PI * x / L);
+  RHO_N2 = rho_N2_0 + rho_N2_x * cos(a_rho_N2_x * PI * x / L);
+  U = u_0 + u_x * sin(a_ux * PI * x / L);
+  T = T_0 + T_x * cos(a_Tx * PI * x / L);
+  kf1_N = Cf1_N * pow(T, etaf1_N) * exp(-Ea_N / R / T);
+  kf1_N2 = Cf1_N2 * pow(T, etaf1_N2) * exp(-Ea_N2 / R / T);
+  Q_rho_N2 = -a_rho_N2_x * PI * rho_N2_x * U * sin(a_rho_N2_x * PI * x / L) / L + a_ux * PI * u_x * RHO_N2 * cos(a_ux * PI * x / L) / L - (0.2e1 * kf1_N * RHO_N + kf1_N2 * RHO_N2) * RHO_N * RHO_N * pow(M_N, -0.2e1) / K_eq + (0.2e1 * kf1_N * RHO_N + kf1_N2 * RHO_N2) * RHO_N2 / M_N / 0.2e1;
+  return(Q_rho_N2);
+}
---
+++ euler_steady_chemistry_1_temperature_K_var/C_code/Euler_1d_steady_chemistry_1T_u_code.C
@@ -0,0 +1,18 @@
+#include <math.h>
+
+double SourceQ_u (double x, double R_N)
+{
+  double Q_u;
+  double RHO;
+  double RHO_N;
+  double RHO_N2;
+  double U;
+  double T;
+  RHO_N = rho_N_0 + rho_N_x * sin(a_rho_N_x * PI * x / L);
+  RHO_N2 = rho_N2_0 + rho_N2_x * cos(a_rho_N2_x * PI * x / L);
+  RHO = RHO_N + RHO_N2;
+  U = u_0 + u_x * sin(a_ux * PI * x / L);
+  T = T_0 + T_x * cos(a_Tx * PI * x / L);
+  Q_u = a_rho_N_x * PI * rho_N_x * U * U * cos(a_rho_N_x * PI * x / L) / L - a_rho_N2_x * PI * rho_N2_x * U * U * sin(a_rho_N2_x * PI * x / L) / L - a_Tx * PI * T_x * R_N * RHO_N * sin(a_Tx * PI * x / L) / L - a_Tx * PI * T_x * R_N * RHO_N2 * sin(a_Tx * PI * x / L) / L / 0.2e1 + 0.2e1 * a_ux * PI * u_x * RHO * U * cos(a_ux * PI * x / L) / L - (-0.2e1 * a_rho_N_x * rho_N_x * cos(a_rho_N_x * PI * x / L) + a_rho_N2_x * rho_N2_x * sin(a_rho_N2_x * PI * x / L)) * PI * R_N * T / L / 0.2e1;
+  return(Q_u);
+}
