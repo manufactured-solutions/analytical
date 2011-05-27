2a3c1d8b8980e6f9a235fadae6a76633c9f2598a
Rhys Ulerich <rhys.ulerich@gmail.com>
2011-05-27 18:17:25 +0000

    masa: #1747 progress on default coeffs

--- navier_stokes_comp_transient_powerlaw/vizcoeffs.py
+++ navier_stokes_comp_transient_powerlaw/vizcoeffs.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python
-# This script is intended to be %run from within IPython.  It provides helpful
-# definitions for interactively visualizing manufactured quantities.
+"""
+SYNOPSIS
 
-from math import pi
+    vizcoeff.py [-h,--help]
+
+DESCRIPTION
+
+    This script is provides helpful definitions for interactively
+    visualizing manufactured quantities within a Mayavi
+    (http://code.enthought.com/projects/mayavi/) session.
+
+VERSION
+
+    $Id$
+"""
+
 import cPickle as pickle
 import sys, os, traceback, optparse, string, textwrap, numpy
 import nsctpl
+from math import pi, sqrt
 from enthought.mayavi import mlab
 
-# Allows programmatically setting module variables
-# http://stackoverflow.com/questions/1429814
-module = sys.modules[__name__]
-
-# Create a manufactured solution instance and populate scenario parameters
+# Create a manufactured solution to which we'll bind nearly everything
 m = nsctpl.manufactured_solution()
-m.gamma    = 1.4
-m.R        = 287
-m.beta     = 2./3.
-m.mu_r     = 185.2e-7
-m.T_r      = 300
-m.k_r      = m.gamma*m.R*m.mu_r/(m.gamma - 1)/0.70
-m.lambda_r = - 2./3. *m.mu_r
-m.Lx       = 4*pi
-m.Ly       = 2
-m.Lz       = 4*pi/3
 
 # Used to programmatically generate ufuncs of the appropriate types
 ufuncs_to_define = [
@@ -117,8 +116,10 @@ def ufunc_generator(name, base):
         return out
     return f
 
+# Programmatically set a module variable for each generated ufunc
+# See http://stackoverflow.com/questions/1429814
 for (name, base, nin, nout) in ufuncs_to_define:
-    setattr(module, name, ufunc_generator(name, base))
+    setattr(sys.modules[__name__], name, ufunc_generator(name, base))
 
 def grid(Nx, Ny, Nz):
     """Create a numpy.mgrid using the solution domain"""
@@ -127,3 +128,96 @@ def grid(Nx, Ny, Nz):
               0 : m.Ly   : Ny * 1j,
         -m.Lz/2 : m.Lz/2 : Nz * 1j
     ]
+
+
+# Establish scenario parameters
+m.gamma    = 1.4
+m.R        = 287
+m.beta     = 2./3.
+m.mu_r     = 185.2e-7
+m.T_r      = 300
+m.k_r      = m.gamma*m.R*m.mu_r/(m.gamma - 1)/0.70
+m.lambda_r = - 2./3. *m.mu_r
+m.Lx       = 4*pi
+m.Ly       = 2
+m.Lz       = 4*pi/3
+
+# Populate primitive solution parameters for an isothermal channel
+
+# Y is the wall-normal direction
+# Want u, v, w, and T constant at the y = {0, Ly} walls
+# Accomplish using phase offset pi / 2 since cos(pi/2) = 0
+for var in [m.u, m.v, m.w, m.T]:
+    for phase in ['c_x', 'c_xy', 'c_xz', 'c_y', 'c_yz', 'c_z',
+                         'e_xy', 'e_xz',        'e_yz',       ]:
+        setattr(var, phase, pi/2)
+
+# Set wavenumbers for all solution variables spatial variations
+for var in [m.rho, m.u, m.v, m.w, m.T]:
+    var.b_x = 3
+    var.b_y = 1
+    var.b_z = 2
+    var.b_xy = var.b_xz = var.b_yz = 1
+    var.d_x = 1
+    var.d_y = 1
+    var.d_z = 1
+    var.d_xy = var.d_xz = var.d_yz = 1
+    var.f_x  = var.f_y  = var.f_z  = 1
+    var.f_xy = var.f_xz = var.f_yz = 1
+
+m.rho.a_0  = 1
+m.rho.a_x  = m.rho.a_0 / 11
+m.rho.a_y  = m.rho.a_0 / 23
+m.rho.a_z  = m.rho.a_0 / 31
+m.rho.a_xy = m.rho.a_0 / 61
+m.rho.a_xz = m.rho.a_0 / 67
+m.rho.a_yz = m.rho.a_0 / 71
+
+m.T.a_0   = m.T_r
+m.T.a_x   = m.T.a_0 / 13
+m.T.a_y   = m.T.a_0 / 29
+m.T.a_z   = m.T.a_0 / 37
+m.T.a_xy  = m.T.a_0 / 101
+m.T.a_xz  = m.T.a_0 / 103
+m.T.a_yz  = m.T.a_0 / 107
+
+m.u.a_0  = 0
+m.u.a_y  = 1.25 * sqrt(m.gamma * m.R * m.T.a_0) # Supersonic
+m.u.a_x  = m.u.a_y / 503
+m.u.a_z  = m.u.a_y / 509
+m.u.a_xy = m.u.a_y / 983
+m.u.a_xz = m.u.a_y / 991
+m.u.a_yz = m.u.a_y / 997
+
+m.v.a_0  = 0
+m.v.a_x  = 2
+m.v.a_y  = 3
+m.v.a_z  = 7
+m.v.a_xx = 2
+m.v.a_xy = 3
+m.v.a_yz = 5
+
+m.w.a_0  = 0
+m.w.a_x  = 2
+m.w.a_y  = 3
+m.w.a_z  = 7
+m.w.a_xx = 2
+m.w.a_xy = 3
+m.w.a_yz = 5
+
+# Compute a default grid based on a coarsening of Coleman et al JFM 1995
+Nx = (144 / 1.5)
+Ny = (119 / 1.5)
+Nz = (80  / 1.5)
+x, y, z = grid(Nx, Ny, Nz)
+
+
+def plotfield(ufunc):
+    mlab.clf()
+    d = ufunc(x, y, z, t)
+    f = mlab.contour3d(x, y, z, d, transparent = True)
+
+## Plot up density field as a starting point
+t = 0
+plotfield(rho)
+
