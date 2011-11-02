9da5853612d820ff4af1d70bc48a11df164e3504
Nicholas Malaya <nicholas.malaya@gmail.com>
2011-11-02 15:31:37 +0000

    [masa]: adding dual number for AD hacking, #2117

---
+++ dualnumber/raw_type.h
@@ -0,0 +1,17 @@
+#ifndef __raw_type_h__
+#define __raw_type_h__
+
+
+template <typename T>
+struct RawType
+{
+  typedef T value_type;
+
+  static value_type value(const T& a) { return a; }
+};
+
+// Make the user syntax slightly nicer
+template <typename T>
+typename RawType<T>::value_type raw_value(const T& a) { return RawType<T>::value(a); }
+
+#endif // __raw_type_h__
